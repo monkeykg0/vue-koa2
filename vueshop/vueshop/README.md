c4bab5c13ffc88fdef507241a641d4e5029ea1c7
monkeykg <monkeykg0@gmail.com>
2018-07-25 07:51:06 +0800

    first commit

--- vueshop/vueshop/README.md
+++ vueshop/vueshop/README.md
@@ -17,5 +17,11 @@ npm run build
 # build for production and view the bundle analyzer report
 npm run build --report
 ```
+<!-- 开发使用的指令如下 -->
+1. 初始化项目 vue init webpack vueshop
+2. 安装vant  sudo cnpm install vant --save
+3. 按需加载 sudo cnpm install babel-plugin-import --save-dev
+4.less 安装 cnpm install less less-loader
+
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ vueshop/vueshop/src/components/pages/ShoppingMall.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class='shopmall'>
+        <!-- search bar -->
+        <div class='search-bar'>
+            <van-row>
+                <van-col span='3'>icon</van-col>
+                <van-col span='16'>search input</van-col>
+                <van-col span='5'> button</van-col>
+            </van-row>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'ShoppingMall',
+    data(){
+        return{
+            msg:'monkeykg'
+        }
+    }
+}
+</script>
+<style lang="less">
+    .shopmall{
+        font-size:0.2rem;
+        .search-bar{
+            height:0.5rem;
+            background: #e5017d;
+            line-height: 0.5rem;
+        }
+    }
+</style>
+
